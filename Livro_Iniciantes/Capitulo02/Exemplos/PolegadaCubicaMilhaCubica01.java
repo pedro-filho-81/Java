156b3faf38021655e00b736a9150e52498bea1a4
PedroFilho <psfilho81@hotmail.com>
2021-11-08 15:54:56 -0300

    Polegadas po Milhas cúbicas

---
+++ Livro_Iniciantes/Capitulo02/Exemplos/PolegadaCubicaMilhaCubica01.java
@@ -0,0 +1,18 @@
+class PolegadaCubicaMilhaCubica
+{
+	public static void main(String[] args)
+	{
+		// variáveis
+		long polegadas;
+		long milhas = 5280 * 12;
+		
+		polegadas = milhas * milhas * milhas;
+		
+		// mostra resultado
+		System.out.println( "Existe " + polegadas + 
+							" cúbicas por milhas cúbicas." );
+		
+		System.out.println("Olá Mundo java!");
+		
+	} // fim main
+} // fim class
