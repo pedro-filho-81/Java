4c505c0b7fb6845ebd485b2628052df1a1383ef5
PedroFilho <psfilho81@hotmail.com>
2021-11-08 18:12:05 -0300

    Caractere como Inteiro

---
+++ Livro_Iniciantes/Capitulo02/Exemplos/CaractereComoInteiro03.java
@@ -0,0 +1,21 @@
+// variáveis do tipo caractere tratada como inteiros
+class CaractereComoInteiro
+{
+	public static void main(String[] args)
+	{
+		// variável
+		char letra = 'X';
+		
+		System.out.println("char letra = " + letra );
+		
+		letra++; // incrementa variável
+		
+		System.out.println("char letra++ -> " + letra );
+		
+		letra = 90; // variável char recebe valor inteiros
+		
+		System.out.println("char letra = 90 -> " + letra );
+		
+		
+	} // fim main
+} // fim class
